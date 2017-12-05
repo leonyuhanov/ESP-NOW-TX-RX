567b84b28eea35ce3f2f3ba12bc2460b1db3b27b
Elec Dash Tron <leon@elec-tron.org>
2017-12-05 21:15:39 +1100

    Add files via upload

---
+++ Receiver/Receiver.ino
@@ -0,0 +1,41 @@
+#include <ESP8266WiFi.h>
+extern "C" {
+  #include <espnow.h>
+}
+
+byte cnt=0;
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.print("\r\n\r\nDevice MAC: ");
+  Serial.println(WiFi.macAddress());
+  Serial.println("\r\nESP_Now Node.");
+  WiFi.mode(WIFI_AP_STA);
+
+  if (esp_now_init() != 0)
+  {
+    Serial.println("*** ESP_Now init failed");
+    ESP.restart();
+  }
+  esp_now_set_self_role(ESP_NOW_ROLE_COMBO);
+    
+  esp_now_register_recv_cb([](uint8_t *mac, uint8_t *data, uint8_t len)
+  {
+    if(data[0]==255)
+    {
+      for(cnt=0; cnt<len; cnt++)
+      {
+        Serial.print(data[cnt], DEC);
+        Serial.print("\t");
+      }
+      Serial.print("\r\n");
+    }
+  });
+}
+
+void loop()
+{
+  
+}
+
---
+++ Transmiter/Transmiter.ino
@@ -0,0 +1,47 @@
+#include <ESP8266WiFi.h>
+extern "C" {
+  #include <espnow.h>
+}
+
+#define WIFI_CHANNEL 1
+
+//MAC ADDRESS OF THE DEVICE YOU ARE SENDING TO
+byte remoteMac[] = {0x60, 0x01, 0x94, 0x1D, 0x49, 0xBC};
+const byte dataLength=7;
+byte cnt=0;
+byte rcvData[dataLength];
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.print("\r\n\r\nDevice MAC: ");
+  Serial.println(WiFi.macAddress());
+  Serial.println("\r\nESP_Now Controller.");
+  WiFi.disconnect();
+
+  if (esp_now_init() != 0)
+  {
+    Serial.println("*** ESP_Now init failed");
+    ESP.restart();
+  }
+  delay(10);
+  
+  esp_now_set_self_role(ESP_NOW_ROLE_CONTROLLER);
+  Serial.println(esp_now_add_peer(remoteMac, ESP_NOW_ROLE_COMBO, WIFI_CHANNEL, NULL, 0));
+}  
+
+void loop()
+{
+  if(rcvData[0]==255)
+  {
+    Serial.printf("\r\nBlock %i",rcvData[0]);
+  }
+  for(cnt=0; cnt<dataLength; cnt++)
+  {
+    rcvData[cnt]++;
+  }
+  esp_now_send(remoteMac, rcvData, dataLength);
+  yield();
+  
+}
+

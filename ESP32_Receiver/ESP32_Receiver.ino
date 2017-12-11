fbd5e1528324667d95ff8d5f1116b5a1ec91f21c
Elec Dash Tron <leon@elec-tron.org>
2017-12-11 14:51:23 +1100

    Add files via upload

---
+++ ESP32_Receiver/ESP32_Receiver.ino
@@ -0,0 +1,31 @@
+/*
+    Compiled for the NODEMCU-32S
+ */
+
+#include <esp_now.h>
+#include <WiFi.h>
+
+#define WIFI_CHANNEL 1
+uint8_t localCustomMac[] = {0x36, 0x33, 0x33, 0x33, 0x33, 0x33};
+const byte maxDataFrameSize = 250;
+byte cnt=0;
+
+void setup()
+{
+  Serial.begin(115200);
+  esp_base_mac_addr_set(localCustomMac);
+  WiFi.mode(WIFI_AP_STA); 
+  esp_now_init();
+  delay(10);
+  esp_now_register_recv_cb(OnDataRecv);
+}
+
+void loop()
+{
+ 
+}
+
+void OnDataRecv(const uint8_t *mac_addr, const uint8_t *data, int data_len)
+{
+  Serial.println(data[0], DEC);
+}
---
+++ ESP32_Transmiter/ESP32_Transmiter.ino
@@ -0,0 +1,37 @@
+/*
+    Compiled for the NODEMCU-32S
+ */
+
+#include <esp_now.h>
+#include <WiFi.h>
+
+#define WIFI_CHANNEL 1
+esp_now_peer_info_t slave;
+uint8_t remoteMac[] = {0x36, 0x33, 0x33, 0x33, 0x33, 0x33};
+const uint8_t maxDataFrameSize=250;
+const esp_now_peer_info_t *peer = &slave;
+uint8_t dataToSend[maxDataFrameSize];
+byte cnt=0;
+
+void setup()
+{
+  Serial.begin(115200);
+  WiFi.disconnect();
+  WiFi.mode(WIFI_STA);
+  esp_now_init();
+  delay(10);
+  memcpy( &slave.peer_addr, &remoteMac, 6 );
+  slave.channel = WIFI_CHANNEL;
+  slave.encrypt = 0;
+  esp_now_add_peer(peer);
+}
+
+void loop()
+{
+  for(cnt=0; cnt<maxDataFrameSize; cnt++)
+  {
+    dataToSend[cnt]++;
+  }
+  esp_now_send(slave.peer_addr, dataToSend, maxDataFrameSize);
+  delay(10);
+}

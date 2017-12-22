fe7c5f281c6305c4268c549469cdb38b1f3d957a
Elec Dash Tron <leon@elec-tron.org>
2017-12-22 12:02:06 +1100

    Update Receiver.ino

--- Receiver/Receiver.ino
+++ Receiver/Receiver.ino
@@ -14,21 +14,14 @@ byte cnt=0;
 void setup()
 {
   Serial.begin(115200);
-  Serial.print("\r\n\r\nDevice MAC: ");
-  Serial.println(WiFi.macAddress());
-  Serial.println("\r\nESP_Now Node.");
-  WiFi.mode(WIFI_AP_STA);
+  Serial.println("\r\nESP_Now MASTER CONTROLLER\r\n");
 
-  if (esp_now_init() != 0)
-  {
-    Serial.println("*** ESP_Now init failed");
-    ESP.restart();
-  }
-  esp_now_set_self_role(ESP_NOW_ROLE_COMBO);
+ esp_now_init();
+ esp_now_set_self_role(ESP_NOW_ROLE_COMBO);
     
   esp_now_register_recv_cb([](uint8_t *mac, uint8_t *data, uint8_t len)
   {
-      Serial.print(".\r\n");
+      Serial.printf("Got Something length =\t%i", len);
   });
 }
 

08060c908dd97af3f02e695723dadce45ec70cf3
Elec Dash Tron <leon@elec-tron.org>
2018-08-15 10:37:03 +1000

    Add files via upload

--- ESP32_Transmiter/ESP32_Transmiter.ino
+++ ESP32_Transmiter/ESP32_Transmiter.ino
@@ -7,8 +7,8 @@
 
 #define WIFI_CHANNEL 1
 esp_now_peer_info_t slave;
-uint8_t remoteMac[] = {0x36, 0x33, 0x33, 0x33, 0x33, 0x33};
-const uint8_t maxDataFrameSize=250;
+uint8_t remoteMac[] = {0x30, 0xAE, 0xA4, 0x27, 0xCE, 0xAD};
+const uint8_t maxDataFrameSize=200;
 const esp_now_peer_info_t *peer = &slave;
 uint8_t dataToSend[maxDataFrameSize];
 byte cnt=0;
@@ -16,14 +16,30 @@ byte cnt=0;
 void setup()
 {
   Serial.begin(115200);
-  WiFi.disconnect();
+  Serial.print("\r\n\r\n");
+  
   WiFi.mode(WIFI_STA);
-  esp_now_init();
-  delay(10);
+  Serial.println( WiFi.softAPmacAddress() );
+  WiFi.disconnect();
+  if(esp_now_init() == ESP_OK)
+  {
+    Serial.println("ESP NOW INIT!");
+  }
+  else
+  {
+    Serial.println("ESP NOW INIT FAILED....");
+  }
+  
+  
   memcpy( &slave.peer_addr, &remoteMac, 6 );
   slave.channel = WIFI_CHANNEL;
   slave.encrypt = 0;
-  esp_now_add_peer(peer);
+  if( esp_now_add_peer(peer) == ESP_OK)
+  {
+    Serial.println("Added Peer!");
+  }
+
+  esp_now_register_send_cb(OnDataSent);
 }
 
 void loop()
@@ -32,6 +48,19 @@ void loop()
   {
     dataToSend[cnt]++;
   }
-  esp_now_send(slave.peer_addr, dataToSend, maxDataFrameSize);
-  delay(10);
+  if( esp_now_send(slave.peer_addr, dataToSend, maxDataFrameSize) == ESP_OK)
+  {
+    Serial.printf("\r\nSuccess Sent Value->\t%d", dataToSend[0]);
+  }
+  else
+  {
+    Serial.printf("\r\nDID NOT SEND....");
+  }
+  delay(250);
+}
+
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status)
+{
+  Serial.print("\r\nLast Packet Send Status:\t");
+  Serial.print(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
 }

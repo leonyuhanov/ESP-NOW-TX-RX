3bb306a2c12b50d0d7f5bc925d33d45181c786b6
Elec Dash Tron <leon@elec-tron.org>
2018-08-15 10:37:20 +1000

    Add files via upload

--- ESP32_Receiver/ESP32_Receiver.ino
+++ ESP32_Receiver/ESP32_Receiver.ino
@@ -7,25 +7,34 @@
 
 #define WIFI_CHANNEL 1
 uint8_t localCustomMac[] = {0x36, 0x33, 0x33, 0x33, 0x33, 0x33};
-const byte maxDataFrameSize = 250;
+const byte maxDataFrameSize = 200;
 byte cnt=0;
 
 void setup()
 {
   Serial.begin(115200);
-  esp_base_mac_addr_set(localCustomMac);
-  WiFi.mode(WIFI_AP_STA); 
-  esp_now_init();
-  delay(10);
+  Serial.print("\r\n\r\n");
+  WiFi.mode(WIFI_AP); 
+  Serial.println( WiFi.softAPmacAddress() );
+  WiFi.disconnect();
+  if(esp_now_init() == ESP_OK)
+  {
+    Serial.println("ESPNow Init Success!");
+  }
+  else
+  {
+    Serial.println("ESPNow Init Failed....");
+  }
   esp_now_register_recv_cb(OnDataRecv);
+  
 }
 
 void loop()
 {
- 
+ yield();
 }
 
 void OnDataRecv(const uint8_t *mac_addr, const uint8_t *data, int data_len)
 {
-  Serial.println(data[0], DEC);
+  Serial.printf("\r\nReceived\t%d Bytes\t%d", data_len, data[0]);
 }
